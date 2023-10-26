746cde9003f573fa2083d6c280b7b707e5830267
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-10-26 13:22:39 -0500

    fix: :sparkles: Add some in Program
    @Gonther1

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Api.Extensions;
+using AspNetCoreRateLimit;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddAplicationServices();
+
+builder.Services.ConfigureRateLimiting();
+
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+
 
 builder.Services.AddDbContext<FarmaCampusContext> (options =>{
     string connectionString = builder.Configuration.GetConnectionString("MySqlConnect");
@@ -23,6 +34,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
+
+app.UseIpRateLimiting();
 
 app.UseHttpsRedirection();
 
