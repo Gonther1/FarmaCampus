319216b28f4479daa8543497b1daa56938e2448f
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-10-26 21:24:34 -0500

    feat: :sparkles: Add ConfigureCors to Program
    @Gonther1

--- Api/Program.cs
+++ Api/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.ConfigureCors();
+
 builder.Services.AddAplicationServices();
 
 builder.Services.ConfigureRateLimiting();
